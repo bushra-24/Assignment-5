5f22d564ff5ae791cf215fbd6ce8ab2148f8caa8
bushra-24 <bushrashakeel77@gmail.com>
2020-06-14 20:46:27 +0500

    ready to commit

---
+++ app.js
@@ -0,0 +1,111 @@
+                                                                  //ALERTS
+                                                                  //CHAPTER # 1
+                                                                  //Task-1
+
+//alert("Welcome Visitors!\nHi! I'm Bushra.\nThank You so much for stopping here. I have something, you probably want to check out.");
+
+                                                                  //TASK-2
+
+ //alert("Error! Please enter a valid password."); 
+ 
+                                                                  //TASK-3
+
+ //alert("Welcome to JS Land...\nHappy Coding! ");  
+ 
+                                                                  //TASK-4
+
+                                                                  //TASK-5
+
+//alert("Welcome to JS Land...");  
+
+                                                                   //TASK-6
+
+//alert("Happy Coding!");  
+
+                                                                   //CHAPTER # 2
+                                                                   //VARIABLES
+                                                                   //TASK-1
+
+//var name ="Username?";
+//alert(name);
+
+                                                                    //TASK-2
+
+//var myName ="Bushra Shakeel!";
+//alert(myName);    
+
+                                                                    //TASK-3
+
+ //var message ="Hello World!";
+ //alert(message);  
+ 
+                                                                    //TASK-4
+
+ //var lastName ="Jhone Doe";
+ //var age ="15 years old";
+ //var experience ="Certified Mobile Application Development";
+ //alert(lastName);
+ //alert(age);
+ //alert(experience);  
+ 
+                                                                     //TASK-5
+
+//var text ="PIZZA\nPIZZ\nPIZ\nPI\nP";  
+//alert(text); 
+
+                                                                      //TASK-6
+
+// var str1 ="My email address is";
+ //var str2 ="bushrashakeel77@gmail.com";
+  //alert(str1 + " " + str2); 
+  
+                                                                      //TASAK-7
+
+ //var book = "\"A smarter way to learn JavaScript\"";
+ //alert(book);                                                                     
+
+                                                                       //TASK-8
+
+ //var str3 ="Yah! i can write HTML";
+ //var str4 ="content through JavaScript.";
+ //var res = str3.concat(str4);
+ //document.getElementById("string").innerHTML = res; 
+ 
+                                                                        //TASK-9
+
+ //var character ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";   
+ //alert(character);
+ 
+                                                                        //CHAPTER # 3
+                                                                        //VARIABLES FOR NUMBERS
+                                                                        //TASK-1
+
+//var age ="I am 22 years old";
+//alert(age);  
+
+                                                                        //TASK-2
+
+//var track = "Dear visitor! you have visited this site 10 times.";
+//alert(track);                                                                        
+
+                                                                        //TASK-3
+
+// var birthYear = "MY birth year is 1998";
+ //var stat = "\nData type of my declared variable is number";
+ //var res = birthYear.concat(stat);
+ //document.getElementById("doc").innerHTML = res; 
+ 
+                                                                        //TASK-4
+
+//var visitorName = "Jhone Doe";
+//var productTitle = "\nordered 5 T-shirt(s) on XYZ Clothing store";
+//document.write("<p>" + visitorName.bold() + productTitle.bold() +"<p>" );
+
+                                                                        //CHAPTER # 4
+                                                                        //VARIABLES NAME:LEGAL & ILLEGAL
+                                                                        //TASK-1
+
+ //var variable1 ="My name is Bushra.",
+   //  variable2 ="I am 22 years old.",
+     //variable3 ="My hobby is Calligraphy"; 
+     //document.write(variable1 + " " + variable2 + " " + variable3);                                                                     
